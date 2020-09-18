15f27505dd64d1381211e2a6d38de6518bb98b52
Matheus Honorio <matheushonorio@eng.ci.ufpb.br>
2020-09-17 21:51:53 -0300

    POST records

---
+++ backend/src/main/java/com/devsuperior/pesquisa/resources/RecordResource.java
@@ -0,0 +1,27 @@
+package com.devsuperior.pesquisa.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.pesquisa.dto.RecordDTO;
+import com.devsuperior.pesquisa.dto.RecordInsertDTO;
+import com.devsuperior.pesquisa.services.RecordService;
+
+@RestController
+@RequestMapping(value = "/records")
+public class RecordResource {
+	
+	@Autowired
+	private RecordService service;
+	
+	@PostMapping
+	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto){
+		RecordDTO newDTO = service.insert(dto);
+		return ResponseEntity.ok().body(newDTO);
+	}
+	
+}
---
+++ backend/src/main/java/com/devsuperior/pesquisa/services/RecordService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.pesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.pesquisa.dto.RecordDTO;
+import com.devsuperior.pesquisa.dto.RecordInsertDTO;
+import com.devsuperior.pesquisa.entities.Game;
+import com.devsuperior.pesquisa.entities.Record;
+import com.devsuperior.pesquisa.repositories.GameRepository;
+import com.devsuperior.pesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+	}
+}
